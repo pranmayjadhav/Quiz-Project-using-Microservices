253c4c62e3904cc989cf08adacf16562f484eb9d
pranm <pranmayj09@gmail.com>
2025-04-01 20:26:48 +0530

    Implementation of Load balancing between multiple service instances

--- QuizService/QuizService/src/main/java/com/quiz/services/QuestionClient.java
+++ QuizService/QuizService/src/main/java/com/quiz/services/QuestionClient.java
@@ -7,8 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+//using fiegn client url
+//@FeignClient(url = "http://localhost:8082", value = "Question-Client")
 
-@FeignClient(url = "http://localhost:8082", value = "Question-Client")
+//using Load Balancer using application name
+@FeignClient(name = "QUESTIONSERVICE")
+
 public interface QuestionClient {
     @GetMapping("/question/quiz/{quizId}")
     List<Question> getQuestionsOfQuiz(@PathVariable Long quizId);
